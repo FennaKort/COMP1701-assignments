113ec462a6c29d7cce3b3da93e49f8794465e21a
Fenna <hbuit817@mtroyal.ca>
2024-09-26 22:56:34 -0600

    completed tests of assn2pt2 content

--- assignment-2/assignment-2-pt2.py
+++ assignment-2/assignment-2-pt2.py
@@ -1,28 +1,35 @@
 # pension calculator inputs:
 RATE = 0.014
+RETIREMENT_AGE_55 = 55
+RETIREMENT_AGE_60 = 60
+RETIREMENT_AGE_65 = 65
 # worker's current age:
-current_age = float(input("What is your current age? "))
+current_age = float(input("enter current age in years: "))
 # worker's current years of service:
-current_years_of_service = float(input("What are your current years of pensionable service? "))
-expected_retirement_age = float(input("What age do you expect to retire at? ")) # for the purposes of assignment2pt1, use age 65
-
-# years of service at retirement calculation:
-years_of_service_at_retirement = expected_retirement_age-current_age+current_years_of_service
-print("At retirement you will have "+str(int(years_of_service_at_retirement))+" years of pensionable service.")
-    # print function to check if variable has been assigned the correct value, not necessary to display to end user - decided to convert it into a print function that WOULD display the information to the end user in a useful way
+current_years_of_service = float(input("enter current years of service: "))
 
 # worker's largest three expected incomes:
-print("What are your three largest expected annual incomes?")
-# three_largest_expected_incomes = input("income 1: $")+input("income 2: $")+input("income 3: $") 
-    # RIGHT because if I do it like this it just concatenates the strings lol
-average_of_three_largest_expected_incomes = (float(input("income 1: $")) + float(input("income 2: $")) + float(input("income 3: $")))/3
+average_of_three_largest_expected_incomes = (float(input("enter the largest expected annual income:")) + float(input("enter the second-largest expected annual income:")) + float(input("enter the third-largest expected annual income:")))/3
 # print(average_of_three_largest_expected_incomes)
     # print function to check if variable has been assigned the correct value, not necessary to display to end user
 
-# pension income calculation:
-pension_income = average_of_three_largest_expected_incomes*RATE*years_of_service_at_retirement
-# print("Your expected yearly pension income at retirement is $"+str(pension_income))
-    # this prints the correct output for the pt1 inputs, but needs to be formatted in order to display the correct level of decimal precision
-    # I previously cast the value assigned to pension_income to a string during assignment, but have decided against that in order to use f-string formatting as per below:
-print(f"Your expected yearly pension income at retirement is ${pension_income:.2f}")
+# pension income if retiring at 55:
+pension_income_retirement_age_55 = average_of_three_largest_expected_incomes*RATE*(RETIREMENT_AGE_55-current_age+current_years_of_service)
+# pension income if retiring at 60:
+pension_income_retirement_age_60 = average_of_three_largest_expected_incomes*RATE*(RETIREMENT_AGE_60-current_age+current_years_of_service)
+# pension income if retiring at 65:
+pension_income_retirement_age_65 = average_of_three_largest_expected_incomes*RATE*(RETIREMENT_AGE_65-current_age+current_years_of_service)
+
+# table headers
+RETIREMENT_AGE_HEADING = "retirement age"
+RETIREMENT_INCOME_HEADING = "retirement income"
+
+# output 
+print(f"{RETIREMENT_AGE_HEADING:20}{RETIREMENT_INCOME_HEADING:20}")
+
+print(f"{RETIREMENT_AGE_55:<20}${pension_income_retirement_age_55:<20,.2f}")
+
+print(f"{RETIREMENT_AGE_60:<20}${pension_income_retirement_age_60:<20,.2f}")
+
+print(f"{RETIREMENT_AGE_65:<20}${pension_income_retirement_age_65:<20,.2f}")
     # prints expected pension income as a float with the correct level of precision to display dollars and cents
